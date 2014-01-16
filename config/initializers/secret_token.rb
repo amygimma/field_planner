6bdaecd4f0d73abf3047250cec089282234bc78f
amyg225 <amyg225@gmail.com>
2014-01-16 15:50:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FieldPlanner::Application.config.secret_key_base = 'a094d7978824f307ee49ca542463651856433bd9f1894bfe26bed706ec13c793c8c6e9673adb997707a7c57986985b3b59293f428c8f29a040cde53589a42860'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FieldPlanner::Application.config.session_store :cookie_store, key: '_field_planner_session'
