fa9c2f6b74eb18e82706f823a79db4fcd883d704
amyg225 <amyg225@gmail.com>
2014-01-22 18:12:19 -0500

    added param greenhouse_start

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,4 +1,5 @@
 class BedsController < ApplicationController
+#needs refacroting, make helpers for crop_id and greenhouse_start/end, etc"
   def show 
     bed_id = params["id"]
     @bed = Bed.find(bed_id) 
@@ -11,12 +12,11 @@ class BedsController < ApplicationController
     crop_id = params["crop_id"]
     
     @crop = Crop.find(crop_id) 
-    
-
-
     @bed = @crop.beds.build(bed_params)
     @bed.save
     
+    @bed.greenhouse_start  = @bed.plant_date - (@crop.greenhouse_time).days
+    @bed.save
     redirect_to bed_path(@bed)
   end
 
@@ -40,6 +40,7 @@ class BedsController < ApplicationController
   def edit
   end
   
+
   private
     def bed_params  
       params.require(:bed).permit(:bed, :frost_date, :greenhouse_start, :greenhouse_end, :harvest, :use_frost, :total_days, :plant_date, :crops_id)
